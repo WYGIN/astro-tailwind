1e594e7804e3f2345ce4ce36b14de80f7117e6a7
WYGIN <wygininc@gmail.com>
2025-01-14 14:11:32 +0530

    install lefthook
    Signed-off-by: WYGIN <wygininc@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,27 @@ import type { AstroIntegration } from "astro";
 import tailwind from "@astrojs/tailwind";
 
 type TailwindOptions = {
-    path: string;
-}
+  path: string;
+};
 
-export default function tailwindIntegration(options: TailwindOptions): AstroIntegration {
-    return {
-        name: 'tailwindcss',
-        hooks: {
-            'astro:config:setup': async ({ config, updateConfig, injectScript }) => {
-                if(!config.integrations.find(({ name }) => name === '@astrojs/tailwind')) {
-                    updateConfig({
-                        vite: {
-                            plugins: [
-                                tailwind({ applyBaseStyles: false }),
-                            ]
-                        }
-                    })
-                }
-                injectScript('page-ssr', `import "${options.path}"`)
+export default function tailwindIntegration(
+  options: TailwindOptions,
+): AstroIntegration {
+  return {
+    name: "tailwindcss",
+    hooks: {
+      "astro:config:setup": async ({ config, updateConfig, injectScript }) => {
+        if (
+          !config.integrations.find(({ name }) => name === "@astrojs/tailwind")
+        ) {
+          updateConfig({
+            vite: {
+              plugins: [tailwind({ applyBaseStyles: false })],
             },
+          });
         }
-    } satisfies AstroIntegration
+        injectScript("page-ssr", `import "${options.path}"`);
+      },
+    },
+  } satisfies AstroIntegration;
 }
